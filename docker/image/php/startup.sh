ee3b13e324dc6da564a9e2dc7baaed32627c4ca8
Roman Bobrik <roman@axeta.ru>
2020-06-28 13:31:13 +0300

    Updated Docker entrypoint implementation (test)

--- docker/image/php/startup.sh
+++ docker/image/php/startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-php-fpm -F -R
-cd /var/www
-echo "Running migrations"
-php artisan migrate
+set -e
+
+echo "Execute custom start-up script"
+#php artisan migrate
+#just for test
+pwd
+ls -la
+
+echo "Execute main"
+exec "$@"
